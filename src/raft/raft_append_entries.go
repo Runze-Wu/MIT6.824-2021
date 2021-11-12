0d8df9cb078c917be2366c31c46862745f69a76e
wurunzenull <181860109@smail.nju.edu.cn>
2021-11-12 18:36:17 +0800

    [update] lab2B all tests passed but need to speed up.

--- src/raft/raft_append_entries.go
+++ src/raft/raft_append_entries.go
@@ -1,11 +1,15 @@
 package raft
 
+import (
+	"sort"
+)
+
 //
 // Helper for sendHeartBeat() to put the right number of entries into the request.
 //
 func (rf *Raft) getAppendLogs(server int) (prevLogIndex, prevLogTerm int, res []Log) {
 	prevLogIndex = rf.nextIndex[server] - 1
-	lastLogIndex, _ := rf.getLastLogIndexTerm()
+	lastLogIndex := rf.getLastLogIndex()
 	if lastLogIndex < prevLogIndex {
 		ERROR("leader %d send hb msg to server %d, but lastLogIndex %d < prevLogIndex %d",
 			rf.me, server, lastLogIndex, prevLogIndex)
@@ -32,6 +36,62 @@ func (rf *Raft) AppendEntries(args *AppendEntriesArgs, reply *AppendEntriesReply
 		reply.Term = args.Term
 	}
 	rf.stepDown(args.Term)
+	rf.setElectionTimer(randomElectionTime())
+
+	// For an entry to fit into our log, it must not leave a gap.
+	if args.PervLogIndex > rf.getLastLogIndex() {
+		VERBOSE("Rejecting AppendEntries RPC: would leave gap")
+		return // response was set to a rejection above
+	}
+
+	if args.PervLogIndex >= rf.getLogStartIndex() &&
+		rf.getLogByIndex(args.PervLogIndex).Term != args.PrevLogTerm {
+		VERBOSE("Rejecting AppendEntries RPC: terms don't agree")
+		return // response was set to a rejection above
+	}
+
+	reply.Success = true // we're accepting the request
+
+	// This needs to be able to handle duplicated RPC requests. We compare the
+	// entries' terms to know if we need to do the operation; otherwise,
+	// reapplying requests can result in data loss.
+	index := args.PervLogIndex
+	for i, entry := range args.Entries {
+		index++
+		if entry.Index != index {
+			ERROR("index not matched")
+		}
+		if index < rf.getLogStartIndex() {
+			// We already snapshotted and discarded this index, so presumably
+			// we've received a committed entry we once already had.
+			rf.printElectionState()
+			ERROR("shouldn't happen before implementing 2D")
+			continue
+		}
+		if rf.getLastLogIndex() >= index {
+			if rf.getLogByIndex(index).Term == entry.Term {
+				continue // avoid due to receive prev request then truncate append entry
+			}
+			if rf.commitIndex >= index {
+				rf.printElectionState()
+				ERROR("should never truncate committed entries")
+			}
+			rf.log = rf.log[:index]
+		}
+		rf.log = append(rf.log, args.Entries[i:]...)
+	}
+
+	// Set our committed ID from the request's. In rare cases, this would make
+	// our committed ID decrease.
+	if rf.commitIndex < args.LeaderCommit {
+		rf.commitIndex = args.LeaderCommit
+		if rf.commitIndex > rf.getLastLogIndex() {
+			rf.printElectionState()
+			ERROR("append RPC: commitIndex larger than log length")
+		}
+		VERBOSE("New commitIndex: %d", rf.commitIndex)
+	}
+
 	rf.setElectionTimer(randomElectionTime())
 }
 
@@ -77,9 +137,19 @@ func (rf *Raft) sendHeartBeat(server int) {
 		}
 
 		if reply.Success {
-
+			if rf.matchIndex[server] > prevLogIndex+len(args.Entries) {
+				WARNING("matchIndex should monotonically increase within a term, " +
+					"since servers don't forget entries. But it didn't.")
+				// stale append RPC could lead to this happen
+			} else {
+				rf.matchIndex[server] = prevLogIndex + len(args.Entries)
+				rf.advanceCommitIndex()
+			}
+			rf.nextIndex[server] = rf.matchIndex[server] + 1
 		} else {
-
+			if rf.nextIndex[server] > 1 {
+				rf.nextIndex[server]--
+			}
 		}
 	}
 	return
@@ -113,4 +183,43 @@ func (rf *Raft) replicate() {
 			go rf.sendHeartBeat(i)
 		}
 	}
+}
+
+//
+//Move forward #commitIndex if possible. Called only on leaders after
+//receiving RPC responses and flushing entries to disk.
+//
+func (rf *Raft) advanceCommitIndex() {
+	if rf.role != Leader {
+		rf.printElectionState()
+		ERROR("non-leader called advanceCommitIndex")
+	}
+	newCommitIndex := quorumMin(rf.matchIndex)
+
+	if newCommitIndex <= rf.commitIndex {
+		return
+	}
+	if newCommitIndex < rf.getLogStartIndex() {
+		// If we have discarded the entry, it's because we already knew it was committed.
+		rf.printElectionState()
+		ERROR("shouldn't happen")
+	}
+	if rf.getLogByIndex(newCommitIndex).Term != rf.currentTerm {
+		// At least one of these entries must also be from the current term to
+		// guarantee that no server without them can be elected.
+		return
+	}
+	rf.commitIndex = newCommitIndex
+	VERBOSE("New commitIndex: %d", rf.commitIndex)
+	if rf.commitIndex > rf.getLastLogIndex() {
+		rf.printElectionState()
+		ERROR("commitIndex larger than log length")
+	}
+}
+
+func quorumMin(matchIndex []int) int {
+	temp := make([]int, len(matchIndex), len(matchIndex))
+	copy(temp, matchIndex)
+	sort.Ints(temp)
+	return temp[(len(temp)-1)/2]
 }
