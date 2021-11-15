4637d855bd8bede09f45e58ce57508dd82bd70c3
wurunzenull <181860109@smail.nju.edu.cn>
2021-11-15 19:54:04 +0800

    [update] persis part list independently.

---
+++ src/raft/raft_persist.go
@@ -0,0 +1,50 @@
+package raft
+
+import (
+	"6.824/labgob"
+	"bytes"
+)
+
+func (rf *Raft) getRaftState() []byte {
+	w := new(bytes.Buffer)
+	e := labgob.NewEncoder(w)
+	e.Encode(rf.currentTerm)
+	e.Encode(rf.voteFor)
+	e.Encode(rf.log)
+	return w.Bytes()
+}
+
+//
+// save Raft's persistent state to stable storage,
+// where it can later be retrieved after a crash and restart.
+// see paper's Figure 2 for a description of what should be persistent.
+//
+func (rf *Raft) persist() {
+	rf.persister.SaveRaftState(rf.getRaftState())
+}
+
+//
+// restore previously persisted state.
+//
+func (rf *Raft) readPersist(data []byte) {
+	if data == nil || len(data) < 1 { // bootstrap without any state?
+		return
+	}
+	r := bytes.NewBuffer(data)
+	d := labgob.NewDecoder(r)
+	var currentTerm int
+	var voteFor int
+	var log []Log
+
+	if d.Decode(&currentTerm) != nil ||
+		d.Decode(&voteFor) != nil ||
+		d.Decode(&log) != nil {
+		ERROR("read persist failed")
+	} else {
+		rf.currentTerm = currentTerm
+		rf.voteFor = voteFor
+		rf.log = log
+		rf.commitIndex = rf.getSnapshot().Index
+		rf.lastApplied = rf.getSnapshot().Index
+	}
+}
